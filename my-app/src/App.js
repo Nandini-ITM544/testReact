acca6d3653e934fae87c9e0731acd97152da9520
nandini_sridharan <nandini.sridharvijay@gmail.com>
2020-12-01 22:53:21 -0500

    final changes with basic axios
    final changes with basic axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import Search from "./search";
 
-import WeatherInformation from "./weatherInformation";
 import Forecast from "./forecast";
 import Footer from "./footer";
 
@@ -11,9 +10,8 @@ function WeatherApp() {
   return (
     <div className='container'>
       <h1>Welcome to Nandini's weather channel 👋🏻</h1>
-      <Search />
+      <Search defaultcity='Paris' />
 
-      <WeatherInformation />
       <div className='forecast-element'>
         <Forecast />
         <Forecast />
--- my-app/src/search.js
+++ my-app/src/search.js
@@ -1,17 +1,29 @@
 import "./search.css";
 import axios from "axios";
 import "./cityTemperature.css";
+import "./weatherInformation.css";
 import React, { useState } from "react";
 
-export default function Search() {
-  const [temperature, setTemperature] = useState(null);
-  const [ready, setReady] = useState(false);
-  const [city, setCity] = useState("Paris");
+export default function Search(props) {
+  const [weatherData, setweatherData] = useState({ ready: false });
   function handleResponse(response) {
     // console.log(response.data.list[0].main.temp);
-    setTemperature(Math.round(response.data.list[0].main.temp));
-    setCity(response.data.city.name);
-    setReady(true);
+    console.log(response.data);
+    setweatherData({
+      ready: true,
+      temperature: Math.round(response.data.list[0].main.temp),
+      humidity: Math.round(response.data.list[0].main.humidity),
+      wind: Math.round(response.data.list[0].wind.speed),
+      realfeel: Math.round(response.data.list[0].main.feels_like),
+      city: response.data.city.name,
+      high_temp: Math.round(response.data.list[0].main.temp_max),
+      low_temp: Math.round(response.data.list[0].main.temp_min),
+      description: response.data.list[0].weather[0].description,
+      // icon:,
+    });
+    //  setTemperature(Math.round(response.data.list[0].main.temp));
+    //  setCity(response.data.city.name);
+    //setReady(true);
     // setTemperature(response.data.main.temp);
   }
   // let apiKey = "fb1865d0d87f6d1b02d912ac727945ca";
@@ -21,7 +33,7 @@ export default function Search() {
   // let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${apiKey}&units=${units}`;
   // axios.get(apiUrl).then(handleResponse);
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div>
         <div className='search'>
@@ -55,10 +67,14 @@ export default function Search() {
           </div>
         </div>
         <div className='city'>
-          <h1 className='cityorzip'>{city}</h1>
+          <h1 className='cityorzip'>{weatherData.city}</h1>
           <h2 id='mainTemperature'>
-            <img id='iconElement' src='' alt='Loading...' />
-            <span id='temp'>{temperature}</span>
+            <img
+              id='iconElement'
+              src={weatherData.icon}
+              alt={weatherData.description}
+            />
+            <span id='temp'>{weatherData.temperature}</span>
             <span className='symbol'>°C</span>
 
             <div className='btn-group btn-group-toggle' data-toggle='buttons'>
@@ -83,14 +99,48 @@ export default function Search() {
             </div>
           </h2>
         </div>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <div className='card information'>
+              <div className='card-body'>
+                <p className='card-text' id='currenttime'></p>
+
+                <p className='card-text'>
+                  Highest for the day:{" "}
+                  <span id='high-temp'>{weatherData.high_temp}°C</span>
+                </p>
+                <p className='card-text'>
+                  Lowest for the day:{" "}
+                  <span id='low-temp'>{weatherData.low_temp}°C</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='col-sm-6'>
+            <div className='card ex information'>
+              <div className='card-body'>
+                <p className='card-text'>
+                  Real Feel:{" "}
+                  <span id='real-feel'>{weatherData.realfeel}°C</span>
+                </p>
+                <p className='card-text'>
+                  Humidity: <span id='humid'>{weatherData.humidity}</span>%
+                </p>
+                <p className='card-text'>
+                  Wind: NE <span id='wind'>{weatherData.wind}</span>mph
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   } else {
     let apiKey = "fb1865d0d87f6d1b02d912ac727945ca";
     //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
-    let currentCity = "New York";
+
     let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.defaultcity}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading......";
   }
