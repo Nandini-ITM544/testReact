98321434e2f2934a6459eed2ae3c15756beecc7d
nandini_sridharan <nandini.sridharvijay@gmail.com>
2020-12-02 17:39:03 -0500

    form functionality and fixed the API URL
    form functionality and fixed the API URL

--- my-app/src/search.js
+++ my-app/src/search.js
@@ -2,25 +2,43 @@ import "./search.css";
 import axios from "axios";
 import "./cityTemperature.css";
 import "./weatherInformation.css";
+import WeatherInformation from "./weatherInformation";
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
 
 export default function Search(props) {
+  function search() {
+    let apiKey = "fb1865d0d87f6d1b02d912ac727945ca";
+    //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
+
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading......";
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   const [weatherData, setweatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultcity);
+  function handleInputChange(event) {
+    setCity(event.target.value);
+  }
   function handleResponse(response) {
     // console.log(response.data.list[0].main.temp);
     console.log(response.data);
     setweatherData({
       ready: true,
-      temperature: Math.round(response.data.list[0].main.temp),
-      humidity: Math.round(response.data.list[0].main.humidity),
-      wind: Math.round(response.data.list[0].wind.speed),
-      realfeel: Math.round(response.data.list[0].main.feels_like),
-      date: new Date(response.data.list[0].dt * 1000),
-      city: response.data.city.name,
-      high_temp: Math.round(response.data.list[0].main.temp_max),
-      low_temp: Math.round(response.data.list[0].main.temp_min),
-      description: response.data.list[0].weather[0].description,
+      temperature: Math.round(response.data.main.temp),
+      humidity: Math.round(response.data.main.humidity),
+      wind: Math.round(response.data.wind.speed),
+      realfeel: Math.round(response.data.main.feels_like),
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+      high_temp: Math.round(response.data.main.temp_max),
+      low_temp: Math.round(response.data.main.temp_min),
+      description: response.data.weather[0].description,
       // icon:,
     });
     //  setTemperature(Math.round(response.data.list[0].main.temp));
@@ -39,7 +57,7 @@ export default function Search(props) {
     return (
       <div>
         <div className='search'>
-          <form className='mb-3' id='search-city'>
+          <form onSubmit={handleSubmit} className='mb-3' id='search-city'>
             <div className='row'>
               <div className='col-9'>
                 <input
@@ -47,6 +65,7 @@ export default function Search(props) {
                   className='form-control'
                   id='cityname'
                   placeholder='Enter a city'
+                  onChange={handleInputChange}
                   // autofocus='on'
                   // autocomplete='on'
                 />
@@ -68,84 +87,11 @@ export default function Search(props) {
             </button>
           </div>
         </div>
-        <div className='city'>
-          <h1 className='cityorzip'>{weatherData.city}</h1>
-          <h2 id='mainTemperature'>
-            <img
-              id='iconElement'
-              src={weatherData.icon}
-              alt={weatherData.description}
-            />
-            <span id='temp'>{weatherData.temperature}</span>
-            <span className='symbol'>°C</span>
-
-            <div className='btn-group btn-group-toggle' data-toggle='buttons'>
-              <label className='btn btn-secondary active' id='celsius'>
-                <input
-                  type='radio'
-                  name='options'
-                  id='option1'
-                  autocomplete='off'
-                />
-                C
-              </label>
-              <label className='btn btn-secondary' id='farenheit'>
-                <input
-                  type='radio'
-                  name='options'
-                  id='option2'
-                  autocomplete='off'
-                />
-                F
-              </label>
-            </div>
-          </h2>
-        </div>
-        <div className='row'>
-          <div className='col-sm-6'>
-            <div className='card information'>
-              <div className='card-body'>
-                <p className='card-text' id='currenttime'>
-                  <FormattedDate defaultdate={weatherData.date} />
-                </p>
-
-                <p className='card-text'>
-                  Highest for the day:{" "}
-                  <span id='high-temp'>{weatherData.high_temp}°C</span>
-                </p>
-                <p className='card-text'>
-                  Lowest for the day:{" "}
-                  <span id='low-temp'>{weatherData.low_temp}°C</span>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className='col-sm-6'>
-            <div className='card ex information'>
-              <div className='card-body'>
-                <p className='card-text'>
-                  Real Feel:{" "}
-                  <span id='real-feel'>{weatherData.realfeel}°C</span>
-                </p>
-                <p className='card-text'>
-                  Humidity: <span id='humid'>{weatherData.humidity}</span>%
-                </p>
-                <p className='card-text'>
-                  Wind: NE <span id='wind'>{weatherData.wind}</span>mph
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherInformation data={weatherData} />
       </div>
     );
   } else {
-    let apiKey = "fb1865d0d87f6d1b02d912ac727945ca";
-    //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
-
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.defaultcity}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading......";
   }
 }
--- my-app/src/weatherInformation.js
+++ my-app/src/weatherInformation.js
@@ -1,35 +1,75 @@
 import React from "react";
 import "./weatherInformation.css";
+import FormattedDate from "./FormattedDate";
 
-export default function WeatherInformation() {
+export default function WeatherInformation(props) {
   return (
-    <div className='row'>
-      <div className='col-sm-6'>
-        <div className='card information'>
-          <div className='card-body'>
-            <p className='card-text' id='currenttime'></p>
+    <div>
+      <div className='city'>
+        <h1 className='cityorzip'>{props.data.city}</h1>
+        <h2 id='mainTemperature'>
+          <img
+            id='iconElement'
+            src={props.data.icon}
+            alt={props.data.description}
+          />
+          <span id='temp'>{props.data.temperature}</span>
+          <span className='symbol'>°C</span>
 
-            <p className='card-text'>
-              Highest for the day: <span id='high-temp'></span>
-            </p>
-            <p className='card-text'>
-              Lowest for the day: <span id='low-temp'></span>
-            </p>
+          <div className='btn-group btn-group-toggle' data-toggle='buttons'>
+            <label className='btn btn-secondary active' id='celsius'>
+              <input
+                type='radio'
+                name='options'
+                id='option1'
+                autocomplete='off'
+              />
+              C
+            </label>
+            <label className='btn btn-secondary' id='farenheit'>
+              <input
+                type='radio'
+                name='options'
+                id='option2'
+                autocomplete='off'
+              />
+              F
+            </label>
+          </div>
+        </h2>
+      </div>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <div className='card information'>
+            <div className='card-body'>
+              <p className='card-text' id='currenttime'>
+                <FormattedDate defaultdate={props.data.date} />
+              </p>
+
+              <p className='card-text'>
+                Highest for the day:{" "}
+                <span id='high-temp'>{props.data.high_temp}°C</span>
+              </p>
+              <p className='card-text'>
+                Lowest for the day:{" "}
+                <span id='low-temp'>{props.data.low_temp}°C</span>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='col-sm-6'>
-        <div className='card ex information'>
-          <div className='card-body'>
-            <p className='card-text'>
-              Real Feel: <span id='real-feel'></span>
-            </p>
-            <p className='card-text'>
-              Humidity: <span id='humid'></span>%
-            </p>
-            <p className='card-text'>
-              Wind: NE <span id='wind'></span>mph
-            </p>
+        <div className='col-sm-6'>
+          <div className='card ex information'>
+            <div className='card-body'>
+              <p className='card-text'>
+                Real Feel: <span id='real-feel'>{props.data.realfeel}°C</span>
+              </p>
+              <p className='card-text'>
+                Humidity: <span id='humid'>{props.data.humidity}</span>%
+              </p>
+              <p className='card-text'>
+                Wind: NE <span id='wind'>{props.data.wind}</span>mph
+              </p>
+            </div>
           </div>
         </div>
       </div>
