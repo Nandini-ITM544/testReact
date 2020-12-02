50eb51ba4bef144862ed107b4716bd4109f6144f
nandini_sridharan <nandini.sridharvijay@gmail.com>
2020-12-01 19:25:14 -0500

    set up the API stuff
    set up the API stuff

--- my-app/src/search.js
+++ my-app/src/search.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./search.css";
+import axios from "axios";
 
 export default function Search() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "fb1865d0d87f6d1b02d912ac727945ca";
+  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
+  let currentCity = "New York";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className='search'>
       <form className='mb-3' id='search-city'>
